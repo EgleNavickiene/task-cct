a3e7cba3459921af26514848736f37dc34067337
EgleNavickiene <navickiene@gmail.com>
2021-11-15 22:44:37 +0200

    Task cct created
    1. new angulat projet task-cct created
    2. bootstrap installed
    3. ngx-bootstrap installed
    4. AccordionModule imported to app.module.ts
    5. task-list component created
    6. Route created in app-routing.module.ts
    7.In task-lis.component.html accordion item created
    8. scss styles added into styles.scss and task-list.component.scss files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
